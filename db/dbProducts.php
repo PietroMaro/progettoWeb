feb8ef9097ae3c6d4089d1924c90fa534de907c0
PietroMaro <pietro.maretti@icloud.com>
2025-11-15 12:49:51 +0100

    Now product are added to the db with the form

--- db/dbProducts.php
+++ db/dbProducts.php
@@ -1,9 +1,78 @@
 <?php
 require_once __DIR__ . '/../db/database.php';
 
-$db = Database::getInstance()->getConnection();
+
+class productManager
+{
+
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
 
 
+
+    public function saveProduct($formData, $loadedFile)
+    {
+
+        $name = $formData['productName'];
+        $description = $formData['productDescription'];
+        $price = $formData['productPrice'];
+
+        if (isset($formData['isAuction'])) {
+            $endDate = $formData['auctionEndDate'];
+
+        } else {
+            $endDate = null;
+        }
+        // $idUtente = $_SESSION['user_id'];
+        $userId = 1; //fino alla creazione del login
+
+
+        $sql = "INSERT INTO prodotto (nome, descrizione, prezzo, stato, ragioneRifiuto,fineAsta, idUtente, idAdmin) 
+                VALUES (?, ?, ?, ?, ?, ?,?,?)";
+
+        $stmt = $this->db->prepare($sql);
+
+        $stmt->execute([
+            $name,
+            $description,
+            $price,
+            'attesa',
+            null,
+            $endDate,
+            $userId,
+            null
+        ]);
+
+        $productId = $this->db->insert_id;
+        $this->saveImages($productId, $loadedFile['images']);
+    }
+
+    public function saveImages($productId, $images)
+    {
+
+        $sql = "INSERT INTO immagini (immagine, idProdotto, idMessaggio) VALUES (?, ?, ?)";
+
+        $stmt = $this->db->prepare($sql);
+
+        foreach ($images['tmp_name'] as $key => $tmp_path) {
+
+
+            $imageData = file_get_contents($tmp_path);
+
+
+            $stmt->execute([
+                $imageData,
+                $productId,
+                null
+            ]);
+
+        }
+    }
+}
 
 
 ?>
--- templates/sellTemplate.php
+++ templates/sellTemplate.php
@@ -1,55 +1,74 @@
-<form action="/vendi" method="POST" enctype="multipart/form-data">
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    try {
+
+        $handler = new productManager();
+        $handler->saveProduct($_POST, $_FILES);
+
+
+        exit;
+
+    } catch (Exception $e) {
+        echo "<h1>Errore</h1>";
+        echo "<p>" . $e->getMessage() . "</p>";
+    }
+}
+
+?>
+
+<form action="" method="POST" enctype="multipart/form-data">
 
     <h1>Unisell</h1>
 
     <ul>
         <li>
-            <label for="astaSwitch">
-                Vuoi che il prodotto sia venduto con un'asta?
+            <label for="auctionSwitch">
+                Vuoi che il prodotto sia venduto con un'asta? 
             </label>
-            <input type="checkbox" role="switch" id="astaSwitch">
-            <small>In questo caso il prezzo inserito sopra sarà considerato la base d'asta</small>
+            <input type="checkbox" role="switch" id="auctionSwitch" name="isAuction">
+            <small>In questo caso il prezzo inserito sarà considerato la base d'asta</small>
         </li>
 
         <li>
-            <label for="nomeProdotto">Nome prodotto</label>
-            <input type="text" id="nomeProdotto" placeholder="Inserisci il nome del prodotto">
+            <label for="productName">Nome del prodotto</label>
+            <input type="text" id="productName" name="productName" placeholder="Inserisci il nome del prodotto" required>
         </li>
 
         <li>
-            <label for="descrizioneProdotto">Descrizione prodotto</label>
-            <textarea id="descrizioneProdotto" rows="3"
-                placeholder="Inserisci una breve descrizione (max 50 parole)"></textarea>
+            <label for="productDescription">Descrizione del prodotto</label>
+            <textarea id="productDescription" name="productDescription" rows="3"
+                placeholder="Inserisci una breve desrizione del prodotto (massimo 50 parole)"></textarea>
         </li>
 
         <li>
-            <label for="prezzoProdotto">Inserisci prezzo</label>
-            <input type="number" id="prezzoProdotto" placeholder="Inserisci prezzo" min="0" step="0.01">
+            <label for="productPrice">Prezzo</label>
+            <input type="number" id="productPrice" name="productPrice" placeholder="Inserisci il prezzo" min="0" step="0.01"
+                required>
         </li>
 
         <li>
             <section>
-                <label>Inserisci immagini</label>
+                <label>Aggiungi immagini</label>
                 <div id="image-preview-wrapper">
                     <span></span><span></span><span></span><span></span>
                 </div>
                 <label for="fileUpload" id="fileUploadLabel">
-                    Aggiungi immagini
+                   Inserisci le immagini
                 </label>
-                <input type="file" id="fileUpload" multiple>
+                <input type="file" id="fileUpload" name="images[]" multiple>
             </section>
         </li>
 
         <li>
-            <label for="dataFine">Inserisci la data e ora di fine dell'asta</label>
-            <input type="datetime-local" id="dataFine">
+            <label for="auctionEndDate">Inserisci la data e ora di fine dell'asta</label>
+            <input type="datetime-local" id="auctionEndDate" name="auctionEndDate">
         </li>
 
         <li>
-
             <button type="button">Modifica</button>
             <button type="submit">Vendi</button>
-
         </li>
     </ul>
 
