7d40eb0df0dd7dba2d3ef94fd437d786a366b8e6
PietroMaro <pietro.maretti@icloud.com>
2025-11-24 21:27:59 +0100

    fixed header error

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,9 @@
 <?php
-require_once __DIR__ . '/db/database.php'; 
-
+ob_start();
+require_once __DIR__ . '/db/database.php';
 require_once __DIR__ . '/db/dbUsers.php';
 require_once __DIR__ . '/db/ProductManager.php';
 require_once __DIR__ . '/db/dbFaq.php';
 
 session_start();
-
-require_once "./db/ProductManager.php";
-require_once "./db/dbUsers.php";
-require_once "./db/dbFaq.php";
-require_once "./db/dbChat.php";
-
 ?>
