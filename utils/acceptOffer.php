8a58f12e3906d7eca569fb1d9700a3caeae0ffee
Tverdohleb Egor <tverdegor@gmail.com>
2025-12-29 12:15:58 +0100

    resetted chat session when accept an offer to avoid a bug regarding cached info

--- utils/acceptOffer.php
+++ utils/acceptOffer.php
@@ -12,6 +12,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $handler = new ProductManager();
     $idChat = $handler->acceptOffer($_POST["chatId"]);
 
+    unset($_SESSION['idChatSelected']);
+    unset($_SESSION['listIdChatSelected']);
+    unset($_SESSION['productNameChatSelected']);
+    unset($_SESSION['productBlobChatSelected']);
+    unset($_SESSION['userNameChatSelected']);
+    unset($_SESSION['userBlobChatSelected']);
+
     header("Location: ../index.php");
 }
 ?>
