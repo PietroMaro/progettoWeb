c8ab5e55d61a5040b4897c1cc8b1466f5250862f
Tverdohleb Egor <tverdegor@gmail.com>
2026-01-05 18:31:41 +0100

    Riadded bootstrap check for signup

--- utils/signup.js
+++ utils/signup.js
@@ -14,3 +14,19 @@ function previewProfileImage(input) {
             reader.readAsDataURL(input.files[0]);
         }
     }
+
+function validateSignUpData () {
+  var forms = document.querySelectorAll('.needs-validation')
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+}
+
+validateSignUpData();
