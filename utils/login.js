969db7167a3b7439bf5dff4e0d84c09375ca1797
Tverdohleb Egor <tverdegor@gmail.com>
2026-01-05 18:33:16 +0100

    Reformatted login js

--- utils/login.js
+++ utils/login.js
@@ -21,15 +21,18 @@ function switchView(view) {
         }
       }
 
-      (function () {
-        var forms = document.querySelectorAll('.needs-validation')
-        Array.prototype.slice.call(forms).forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-              if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-              }
-              form.classList.add('was-validated')
-            }, false)
-        })
-})()
+function validateLoginData () {
+  var forms = document.querySelectorAll('.needs-validation')
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+}
+
+validateLoginData();
