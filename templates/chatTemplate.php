e0a21acd236de784ed7bb1b2f8d27e6a5c1e661f
Tverdohleb Egor <tverdegor@gmail.com>
2026-01-07 20:40:10 +0100

    -

--- templates/chatTemplate.php
+++ templates/chatTemplate.php
@@ -80,6 +80,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['new_selected_chat_list
     } elseif ($chatStatus === "astaDeserta") {
         $chatFinished = true;
         $disableOfferButton = true;
+    } else {
+        $disableOfferButton = false;
     }
 
 }
